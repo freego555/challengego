84dd3d9ba85985ba9be0ab2a739b483801d4ab7d
Ihor Rohovets <freego555@gmail.com>
2020-04-23 17:33:45 +0300

    Contract Challenge. Functions addChallenge(), addToSchedule(), startChallenge(). Changes in gitignore

--- smart_contracts/contracts/Challenge.sol
+++ smart_contracts/contracts/Challenge.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.6;
 
 contract Challenge {
     uint256 lastChallengeId;
@@ -17,8 +17,40 @@ contract Challenge {
     mapping(uint256 => uint256) start; // [challengeId] = date of start of challenge
     mapping(uint256 => mapping(uint256 => uint256)) schedule; // [challengeId][indexOfItem] = duration of every item of schedule
     mapping(uint256 => uint256) lastScheduleItemId; // [challengeId] = index of last schedule item
-    mapping(uint256 => uint256) idOfCurrentScheduleItem; // [challengeId] = index of current schedule item
-    mapping(uint256 => uint256) startTimeOfCurrentScheduleItem; // [challengeId] = date of start of current item of schedule
-    mapping(uint256 => mapping(uint256 => bool)) scheduleDone; // [challengeId][indexOfItem] = result of item of schedule (done or not)
-    mapping(uint256 => mapping(uint256 => bool)) scheduleFineTaken; // [challengeId][indexOfItem] = Is fine of item of schedule taken or not?
+    mapping(uint256 => uint256) idOfCurrentScheduleItem; // [challengeId] = index of current schedule item (stage of doing challenge)
+    mapping(uint256 => uint256) startTimeOfCurrentScheduleItem; // [challengeId] = date of start of current item of schedule (stage of doing challenge)
+    mapping(uint256 => mapping(uint256 => bool)) scheduleDone; // [challengeId][indexOfItem] = result of item of schedule (done or not) (stage of doing challenge)
+    mapping(uint256 => mapping(uint256 => bool)) scheduleFineTaken; // [challengeId][indexOfItem] = Is fine of item of schedule taken or not? (stage of doing challenge)
+
+    function addChallenge(uint256 _start, address _observer, uint256 _guarantee, uint256 _fine) public {
+        require(_guarantee >= _fine, "Sum of guarantee should be either equal or greater than sum of fine");
+
+        lastChallengeId++;
+
+        owner[lastChallengeId] = msg.sender;
+
+        start[lastChallengeId] = _start;
+
+        lastAchieverId[lastChallengeId]++;
+        achievers[lastChallengeId][lastAchieverId[lastChallengeId]] = msg.sender;
+
+        lastObserverId[lastChallengeId]++;
+        observers[lastChallengeId][lastObserverId[lastChallengeId]] = _observer;
+
+        guarantee[lastChallengeId][lastAchieverId[lastChallengeId]] = _guarantee;
+        fine[lastChallengeId][lastAchieverId[lastChallengeId]] = _fine;
+    }
+
+    function addToSchedule(uint256 _challengeId, uint256[10] memory _scheduleItems) public {
+        require(msg.sender == owner[_challengeId], "Only owner of challenge can call this");
+
+        uint256 nextScheduleItemId = 0;
+        for(uint256 i = 0; i < 10; i++) {
+            nextScheduleItemId = ++lastScheduleItemId[_challengeId];
+            schedule[_challengeId][nextScheduleItemId] = _scheduleItems[i];
+        }
+    }
+
+    function startChallenge() public {
+    }
 }
