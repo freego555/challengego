ed1b742a3e654cf47c0b857a6d1ce13b88cc8df5
Ihor Rohovets <freego555@gmail.com>
2020-05-06 12:53:06 +0300

    Refactoring. Every occurence of 'schedule item' is replaced to 'schedule period'

--- smart_contracts/contracts/Challenge.sol
+++ smart_contracts/contracts/Challenge.sol
@@ -15,13 +15,13 @@ contract Challenge {
     mapping(uint256 => mapping(uint256 => uint256)) fine; // [challengeId][indexOfAchiever] = sum of fine in wei
 
     mapping(uint256 => uint256) start; // [challengeId] = date of start of challenge
-    mapping(uint256 => mapping(uint256 => uint256)) schedule; // [challengeId][indexOfItem] = duration of every item of schedule in seconds
-    mapping(uint256 => uint256) lastScheduleItemId; // [challengeId] = index of last schedule item
-    mapping(uint256 => uint256) idOfCurrentScheduleItem; // [challengeId] = index of current schedule item (stage of doing challenge)
-    mapping(uint256 => uint256) startTimeOfCurrentScheduleItem; // [challengeId] = date of start of current item of schedule (stage of doing challenge)
-    mapping(uint256 => mapping(uint256 => bool)) scheduleDone; // [challengeId][indexOfItem] = result of item of schedule (done or not) (stage of doing challenge)
+    mapping(uint256 => mapping(uint256 => uint256)) schedule; // [challengeId][indexOfItem] = duration of every schedule period in seconds
+    mapping(uint256 => uint256) lastSchedulePeriodId; // [challengeId] = index of last schedule period
+    mapping(uint256 => uint256) idOfCurrentPeriod; // [challengeId] = index of current schedule period (stage of doing challenge)
+    mapping(uint256 => uint256) startTimeOfCurrentPeriod; // [challengeId] = date of start of current schedule period (stage of doing challenge)
+    mapping(uint256 => mapping(uint256 => bool)) scheduleDone; // [challengeId][indexOfItem] = result of schedule period (done or not) (stage of doing challenge)
 
-    mapping(uint256 => mapping(uint256 => uint256)) scheduleFineAvailable; // [challengeId][index] = index of period of schedule for what fine taking is available
+    mapping(uint256 => mapping(uint256 => uint256)) scheduleFineAvailable; // [challengeId][index] = index of schedule period for what fine taking is available
     mapping(uint256 => uint256) lastIdOfScheduleFineAvailable; // [challengeId] = last index of mapping scheduleFineAvailable
 
     function addChallenge(uint256 _start, address _observer, uint256 _guarantee, uint256 _fine) public {
@@ -43,13 +43,13 @@ contract Challenge {
         fine[lastChallengeId][lastAchieverId[lastChallengeId]] = _fine;
     }
 
-    function addToSchedule(uint256 _challengeId, uint256[10] memory _scheduleItems) public {
-        require(msg.sender == owner[_challengeId], "Only owner of challenge can add item to schedule");
+    function addToSchedule(uint256 _challengeId, uint256[10] memory _schedulePeriods) public {
+        require(msg.sender == owner[_challengeId], "Only owner of challenge can add period to schedule");
 
         uint256 nextScheduleItemId = 0;
         for(uint256 i = 0; i < 10; i++) {
-            nextScheduleItemId = ++lastScheduleItemId[_challengeId];
-            schedule[_challengeId][nextScheduleItemId] = _scheduleItems[i];
+            nextScheduleItemId = ++lastSchedulePeriodId[_challengeId];
+            schedule[_challengeId][nextScheduleItemId] = _schedulePeriods[i];
         }
     }
 
@@ -61,31 +61,31 @@ contract Challenge {
             start[_challengeId] = now;
         }
 
-        idOfCurrentScheduleItem[_challengeId]++;
-        startTimeOfCurrentScheduleItem[_challengeId] = start[_challengeId];
+        idOfCurrentPeriod[_challengeId]++;
+        startTimeOfCurrentPeriod[_challengeId] = start[_challengeId];
     }
 
     function setDoneForPeriod(uint256 _challengeId) public {
         require(lastChallengeId >= _challengeId, "Challenge ID doesn't exist");
         require(msg.sender == achievers[_challengeId][1], "Only achiever of challenge can set done for period");
 
-        uint256 endTimeOfCurrentScheduleItem = startTimeOfCurrentScheduleItem[_challengeId] + schedule[_challengeId][idOfCurrentScheduleItem[_challengeId]];
-        require(endTimeOfCurrentScheduleItem >= now, "Current schedule item has already completed. At first you need to use function calcCurrentScheduleItem()");
+        uint256 endTimeOfCurrentScheduleItem = startTimeOfCurrentPeriod[_challengeId] + schedule[_challengeId][idOfCurrentPeriod[_challengeId]];
+        require(endTimeOfCurrentScheduleItem >= now, "Current schedule period has already completed. At first you need to use function calcCurrentScheduleItem()");
 
-        scheduleDone[_challengeId][idOfCurrentScheduleItem[_challengeId]] = true;
+        scheduleDone[_challengeId][idOfCurrentPeriod[_challengeId]] = true;
 
-        // Start new schedule item
-        idOfCurrentScheduleItem[_challengeId]++;
-        startTimeOfCurrentScheduleItem[_challengeId] = endTimeOfCurrentScheduleItem + 1;
+        // Start new schedule period
+        idOfCurrentPeriod[_challengeId]++;
+        startTimeOfCurrentPeriod[_challengeId] = endTimeOfCurrentScheduleItem + 1;
     }
 
     function calcCurrentPeriod(uint256 _challengeId) public returns(bool) {
-        require(startTimeOfCurrentScheduleItem[_challengeId] + schedule[_challengeId][idOfCurrentScheduleItem[_challengeId]] > now, "Current schedule item is actual.");
+        require(startTimeOfCurrentPeriod[_challengeId] + schedule[_challengeId][idOfCurrentPeriod[_challengeId]] > now, "Current schedule period is actual.");
 
         uint256 sizeOfChunk = 10; // limit amount of periods to avoid exceeding limit of gas
         bool isActual = false;
-        for (uint256 i = idOfCurrentScheduleItem[_challengeId]; i <= idOfCurrentScheduleItem[_challengeId] + sizeOfChunk; i++) {
-            if (startTimeOfCurrentScheduleItem[_challengeId] + schedule[_challengeId][i] <= now) {
+        for (uint256 i = idOfCurrentPeriod[_challengeId]; i <= idOfCurrentPeriod[_challengeId] + sizeOfChunk; i++) {
+            if (startTimeOfCurrentPeriod[_challengeId] + schedule[_challengeId][i] <= now) {
                 if (scheduleDone[_challengeId][i] == false) {
                     // Add element to scheduleFineAvailable. Observer can get fine for this period.
                     lastIdOfScheduleFineAvailable[_challengeId]++;
@@ -93,8 +93,8 @@ contract Challenge {
                 }
 
                 // Go to the next period
-                startTimeOfCurrentScheduleItem[_challengeId] += schedule[_challengeId][i];
-                idOfCurrentScheduleItem[_challengeId]++;
+                startTimeOfCurrentPeriod[_challengeId] += schedule[_challengeId][i];
+                idOfCurrentPeriod[_challengeId]++;
             } else {
                 // Current period is actual
                 isActual = true;
